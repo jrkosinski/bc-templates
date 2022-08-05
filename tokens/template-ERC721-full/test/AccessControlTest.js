10dfac966a3a811d5e895032edd8e43c151ab3df
Acer <john.kosinski@gmail.com>
2022-08-06 00:59:02 +0700

    more unit tests for access control

--- tokens/template-ERC721-full/test/AccessControlTest.js
+++ tokens/template-ERC721-full/test/AccessControlTest.js
@@ -5,9 +5,7 @@ const deploy = require("./util/deploy");
 const testEvent = require("./util/testEvent");
 
 //TODO: make sure every error case is covered 
-//TODO: make sure all access control cases are covered 
 //TODO: add revertedWith with error msg variables 
-//TODO: remove all unused requires
 
 describe(constants.TOKEN_CONTRACT_ID + ": Access Control", function () {		  
 	let nft;				    //contracts
@@ -71,49 +69,67 @@ describe(constants.TOKEN_CONTRACT_ID + ": Access Control", function () {
     }); 
     
 	describe("Admin Role", function () {
+        let admin, nonAdmin; 
+        
         beforeEach(async function () {
             await nft.grantRole(constants.roles.ADMIN, addr1.address); 
+            admin = addr1;
+            nonAdmin = addr2; 
         });
         
 		it("non-admin cannot grant role", async function () {
-            await expect(nft.connect(addr2).grantRole(constants.roles.MINTER, addr1.address)).to.be.reverted;
-            await expect(nft.connect(addr1).grantRole(constants.roles.MINTER, addr1.address)).to.not.be.reverted;
+            await expect(nft.connect(nonAdmin).grantRole(constants.roles.MINTER, admin.address)).to.be.reverted;
+            await expect(nft.connect(admin).grantRole(constants.roles.MINTER, admin.address)).to.not.be.reverted;
 		});
         
 		it("non-admin cannot revoke role", async function () {
-            await expect(nft.connect(addr2).revokeRole(constants.roles.MINTER, owner.address)).to.be.reverted;
-            await expect(nft.connect(addr1).revokeRole(constants.roles.MINTER, owner.address)).to.not.be.reverted;
+            await expect(nft.connect(nonAdmin).revokeRole(constants.roles.MINTER, owner.address)).to.be.reverted;
+            await expect(nft.connect(admin).revokeRole(constants.roles.MINTER, owner.address)).to.not.be.reverted;
 		});
         
 		it("non-admin cannot pause", async function () {
-            await expect(nft.connect(addr2).pause()).to.be.reverted;
-            await expect(nft.connect(addr1).pause()).to.not.be.reverted;
-		});
+            await expect(nft.connect(nonAdmin).pause()).to.be.reverted;
+            await expect(nft.connect(admin).pause()).to.not.be.reverted;
+        });
+
+        it("non-admin cannot unpause", async function () {
+            await nft.connect(addr1).pause();
+            await expect(nft.connect(nonAdmin).unpause()).to.be.reverted;
+            await expect(nft.connect(admin).unpause()).to.not.be.reverted;
+        });
         
 		it("non-admin cannot set supply parameters", async function () {
-            await expect(nft.connect(addr2).setSupplyParameters(10, 10)).to.be.reverted;
-            await expect(nft.connect(addr1).setSupplyParameters(10, 10)).to.not.be.reverted;
+            await expect(nft.connect(nonAdmin).setSupplyParameters(10, 10)).to.be.reverted;
+            await expect(nft.connect(admin).setSupplyParameters(10, 10)).to.not.be.reverted;
         });
         
 		it("non-admin set base URI", async function () {
-            await expect(nft.connect(addr2).setBaseUri("uri")).to.be.reverted;
-            await expect(nft.connect(addr1).setBaseUri("uri")).to.not.be.reverted;
+            await expect(nft.connect(nonAdmin).setBaseUri("uri")).to.be.reverted;
+            await expect(nft.connect(admin).setBaseUri("uri")).to.not.be.reverted;
 		});
 
         it("non-admin cannot set royalty info", async function () {
-            await expect(nft.connect(addr2).setRoyaltyInfo(addr1.address, 3, 1000)).to.be.reverted;
-            await expect(nft.connect(addr1).setRoyaltyInfo(addr1.address, 3, 1000)).to.not.be.reverted;
+            await expect(nft.connect(nonAdmin).setRoyaltyInfo(admin.address, 3, 1000)).to.be.reverted;
+            await expect(nft.connect(admin).setRoyaltyInfo(admin.address, 3, 1000)).to.not.be.reverted;
         });
 
         it("non-admin cannot clear royalty info", async function () {
-            await expect(nft.connect(addr2).clearRoyaltyInfo()).to.be.reverted;
-            await expect(nft.connect(addr1).clearRoyaltyInfo()).to.not.be.reverted;
+            await expect(nft.connect(nonAdmin).clearRoyaltyInfo()).to.be.reverted;
+            await expect(nft.connect(admin).clearRoyaltyInfo()).to.not.be.reverted;
+        });
+
+        it("non-admin cannot call initialMint", async function () {
+            expect(await nft.totalSupply()).to.equal(0); 
+            
+            await expect(nft.connect(nonAdmin).initialMint()).to.be.reverted;
+            await expect(nft.connect(admin).initialMint()).to.not.be.reverted;
         });
     }); 
     
 	describe("Minter Role", function () {
+        
 		it("initial owner/admin can mint", async function () {
-			await nft.mintNext(owner.address); 
+            await nft.mintNext(owner.address); 
             expect(await nft.balanceOf(owner.address)).to.equal(1); 
 		});
         
@@ -121,11 +137,28 @@ describe(constants.TOKEN_CONTRACT_ID + ": Access Control", function () {
 			await expect(nft.connect(addr1).mintNext(addr1.address)).to.be.reverted;
 		});
         
-		it("minter role can mint", async function () {
-			await(nft.grantRole(constants.roles.MINTER, addr1.address)); 
-			await nft.connect(addr1).mintNext(addr1.address); 
-            expect(await nft.balanceOf(addr1.address)).to.equal(1); 
-		});
+        it("minter role can mint", async function () {
+            const minter = addr1; 
+            
+            await (nft.grantRole(constants.roles.MINTER, minter.address)); 
+            await nft.connect(minter).mintNext(minter.address); 
+            expect(await nft.balanceOf(minter.address)).to.equal(1); 
+        });
+
+        it("minter role can mint multiple", async function () {
+            const minter = addr1; 
+            
+            await (nft.grantRole(constants.roles.MINTER, minter.address));
+            await nft.connect(minter).multiMint(minter.address, 3);
+            expect(await nft.balanceOf(minter.address)).to.equal(3);
+        });
+
+        it("minter role cannot initialMint", async function () {
+            const minter = addr1;
+
+            await (nft.grantRole(constants.roles.MINTER, minter.address));
+            await expect(nft.connect(minter).initialMint()).to.be.reverted;
+        });
     });
 
     describe("Events", function () {
